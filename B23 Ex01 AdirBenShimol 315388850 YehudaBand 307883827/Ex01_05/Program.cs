90dd858788fe69415054ece2e734f38a7c6d92d5
Ybyb2919 <yehudaband@gmail.com>
2023-04-21 20:12:13 +0300

    initial commit ex01_05

--- B23 Ex01 AdirBenShimol 315388850 YehudaBand 307883827/Ex01_05/Program.cs
+++ B23 Ex01 AdirBenShimol 315388850 YehudaBand 307883827/Ex01_05/Program.cs
@@ -1,4 +1,8 @@
-﻿namespace Ex01_05
+﻿//TODO: import Ex01_01.GetInput(string i_DialogMessage, Func i_InputValidator, out o_UserInput)
+
+using System;
+
+namespace Ex01_05
 {
     class Program
     {
@@ -6,5 +10,44 @@
         {
 
         }
+
+        static void RunProgram()
+        {
+            Ex01_01.GetInput(string i_DialogMessage, Func IsNDigitsNumber, out o_UserInput)
+
+        }
+
+        static void UserInterface()
+        {
+
+        }
+
+        static bool IsNDigitsNumber(string i_InputNumber, int i_NumberOfDigits)
+        {
+            //Use int.TryParse
+        }
+
+        static int GetRightmostDigit(int[] i_InputNumberArray)
+        {
+
+        }
+
+        static int GetLowestDigit(int[] i_InputNumberArray)
+        {
+
+        }
+
+        static float GetDigitAverage(int[] i_InputNumberArray)
+        {
+
+        }
+
+
+
+
+
+
+
+
     }
 }
