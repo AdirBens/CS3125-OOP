71568f4a5ab2fbbd3c43c94cc3cf1ac5b22f5be8
Ybyb2919 <yehudaband@gmail.com>
2023-04-23 12:46:59 +0300

    changed method names

--- B23 Ex01 AdirBenShimol 315388850 YehudaBand 307883827/Ex01_05/Program.cs
+++ B23 Ex01 AdirBenShimol 315388850 YehudaBand 307883827/Ex01_05/Program.cs
@@ -10,35 +10,35 @@ namespace Ex01_05
     {
         public static void Main()
         {
-            RunProgram();
+            runProgram();
         }
 
-        static void RunProgram()
+        static void runProgram()
         {
-            const string v_DialogMessage = "Please enter a 6 digit number and hit enter :";
-            const string v_InvalidInputMessage = "Invalid input! Please enter a 6 digit number with no letters or symbols.";
+            const string k_DialogMessage = "Please enter a 6 digit number and hit enter :";
+            const string k_InvalidInputMessage = "Invalid input! Please enter a 6 digit number with no letters or symbols.";
 
-            GetInput(v_DialogMessage, StringIsSixDigitsNumber, v_InvalidInputMessage, out string userInput);
-            PrintStatistics(userInput);
+            GetInput(k_DialogMessage, stringIsSixDigitsNumber, k_InvalidInputMessage, out string userInput);
+            printStatistics(userInput);
         }
 
-        static bool StringIsSixDigitsNumber(string i_UserInput)
+        static bool stringIsSixDigitsNumber(string i_UserInput)
         {
             return (StringIsLengthSix(i_UserInput) && StringIsNumber(i_UserInput));
         }
 
-        static void PrintStatistics(string i_UserInput)
+        static void printStatistics(string i_UserInput)
         { 
             Console.WriteLine(string.Format("The number you chose is: {0}", i_UserInput));
             Console.WriteLine(string.Format("{0} digits from the number are greater than the rightmost digit.", 
-                GetNumberOfDigitsLargerThanRightmostDigit(i_UserInput)));
-            Console.WriteLine(string.Format("The smallest digit in the number is: {0}", GetLowestDigit(i_UserInput)));
-            Console.WriteLine(string.Format("The average of all digits is: {0}", GetDigitAverage(i_UserInput)));   
+                getNumberOfDigitsLargerThanRightmostDigit(i_UserInput)));
+            Console.WriteLine(string.Format("The smallest digit in the number is: {0}", getLowestDigit(i_UserInput)));
+            Console.WriteLine(string.Format("The average of all digits is: {0}", getDigitAverage(i_UserInput)));   
             Console.WriteLine("Hit ENTER to end the program...");
             Console.ReadLine();
         }
 
-        static int GetNumberOfDigitsLargerThanRightmostDigit(string i_UserInput)
+        static int getNumberOfDigitsLargerThanRightmostDigit(string i_UserInput)
         {
             int rightmostDigit = i_UserInput[i_UserInput.Length - 1];
             int numberOfLargerThanRightmostDigit = 0;
@@ -52,7 +52,7 @@ namespace Ex01_05
             return numberOfLargerThanRightmostDigit;
         }
 
-        static char GetLowestDigit(string i_UserInput)
+        static char getLowestDigit(string i_UserInput)
         {
             char smallestDigit = i_UserInput[0];
             foreach(char c in i_UserInput)
@@ -65,7 +65,7 @@ namespace Ex01_05
             return smallestDigit;
         }
 
-        static float GetDigitAverage(string i_userInput)
+        static float getDigitAverage(string i_userInput)
         {
             float sumOfDigits = 0;
             foreach(char c in i_userInput)
