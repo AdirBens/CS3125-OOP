076671b371388ecb3f7283d97a0178f5ab575dfc
BenshimolAdir <benshimol.adir@post.runi.ac.il>
2023-06-13 04:13:38 +0300

    Implement Interfaces Approach

---
+++ B23 Ex04 Adir 315388850 Yehuda 307883827/Ex04.Menus.Test/InterfacesApproach/ShowVersionActionable.cs
@@ -0,0 +1,14 @@
+﻿
+using Ex04.Menus.Interfaces;
+using Ex04.Menus.Test.TestUtils;
+
+namespace Ex04.Menus.Test.InterfacesApproach
+{
+    internal class ShowVersionActionable : ISelectedObservers
+    {
+        public void NotifyItemSelected()
+        {
+            VersionAndCapitalsAgent.ShowVersion();
+        }
+    }
+}
--- B23 Ex04 Adir 315388850 Yehuda 307883827/Ex04.Menus.Test/Program.cs
+++ B23 Ex04 Adir 315388850 Yehuda 307883827/Ex04.Menus.Test/Program.cs
@@ -1,11 +1,15 @@
 ﻿
+using static Ex04.Menus.Test.InterfacesApproach.InterfacesMainMenu;
+
 namespace Ex04.Menus.Test
 {
     public class Program
     {
         public static void Main()
         {
+            Interfaces.MainMenu interfacesMainMenu = GetInterfacesMainMenu();
 
+            interfacesMainMenu.Show();
         }
     }
 }
